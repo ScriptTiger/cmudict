0822b90abf536092846f17d66279944dd0f7849a
ScriptTiger <thescripttiger@gmail.com>
2018-01-11 08:39:31 +0800

    Update search.cmd

--- search.cmd
+++ search.cmd
@@ -15,7 +15,7 @@ goto skip_license
 
 MIT License
 
-Copyright (c) 2017 ScriptTiger
+Copyright (c) 2018 ScriptTiger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
